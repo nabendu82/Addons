4f548904c05a77bf5b1e7f64eb420121f6bb528f
nabendu82 <nabendu.biswas@gmail.com>
2020-04-22 23:27:41 +0530

    Tab Utility complete

--- TabUtility/tabs.js
+++ TabUtility/tabs.js
@@ -1,3 +1,8 @@
+const ZOOM_INCREMENT = 0.2;
+const MAX_ZOOM = 3;
+const MIN_ZOOM = 0.3;
+const DEFAULT_ZOOM = 1;
+
 function listTabs() {
     getCurrentWindowTabs().then((tabs) => {
         let tabsList = document.getElementById('tabs-list');
@@ -52,6 +57,78 @@ document.addEventListener("click", (e) => {
                 index = lastPinnedTab;
             }
             browser.tabs.move([tab.id], { index });
+        });
+    } else if (e.target.id === "tabs-duplicate") {
+        callOnActiveTab((tab) => {
+            browser.tabs.duplicate(tab.id);
+        });
+    } else if (e.target.id === "tabs-reload") {
+        callOnActiveTab((tab) => {
+            browser.tabs.reload(tab.id);
+        });
+    } else if (e.target.id === "tabs-remove") {
+        callOnActiveTab((tab) => {
+            browser.tabs.remove(tab.id);
+        });
+    } else if (e.target.id === "tabs-create") {
+        browser.tabs.create({});
+    } else if (e.target.id === "tabs-alertinfo") {
+        callOnActiveTab((tab) => {
+            let props = "";
+            for (let item in tab) {
+                props += `${item} = ${tab[item]} \n`;
+            }
+            alert(props);
+        });
+    } else if (e.target.id === "tabs-add-zoom") {
+        callOnActiveTab((tab) => {
+            var gettingZoom = browser.tabs.getZoom(tab.id);
+            gettingZoom.then((zoomFactor) => {
+                if (zoomFactor >= MAX_ZOOM) {
+                    alert("Tab zoom factor is already at max!");
+                } else {
+                    var newZoomFactor = zoomFactor + ZOOM_INCREMENT;
+                    newZoomFactor = newZoomFactor > MAX_ZOOM ? MAX_ZOOM : newZoomFactor;
+                    browser.tabs.setZoom(tab.id, newZoomFactor);
+                }
+            });
+        });
+    } else if (e.target.id === "tabs-decrease-zoom") {
+        callOnActiveTab((tab) => {
+            var gettingZoom = browser.tabs.getZoom(tab.id);
+            gettingZoom.then((zoomFactor) => {
+                if (zoomFactor <= MIN_ZOOM) {
+                    alert("Tab zoom factor is already at minimum!");
+                } else {
+                    var newZoomFactor = zoomFactor - ZOOM_INCREMENT;
+                    newZoomFactor = newZoomFactor < MIN_ZOOM ? MIN_ZOOM : newZoomFactor;
+                    browser.tabs.setZoom(tab.id, newZoomFactor);
+                }
+            });
+        });
+    } else if (e.target.id === "tabs-default-zoom") {
+        callOnActiveTab((tab) => {
+            var gettingZoom = browser.tabs.getZoom(tab.id);
+            gettingZoom.then((zoomFactor) => {
+                if (zoomFactor == DEFAULT_ZOOM) {
+                    alert("Tab zoom is already at the default zoom factor");
+                } else {
+                    browser.tabs.setZoom(tab.id, DEFAULT_ZOOM);
+                }
+            });
+        });
+    } else if (e.target.classList.contains('switch-tabs')) {
+        var tabId = +e.target.getAttribute('href');
+        browser.tabs.query({
+            currentWindow: true
+        }).then((tabs) => {
+            for (var tab of tabs) {
+                if (tab.id === tabId) {
+                    browser.tabs.update(tabId, {
+                        active: true
+                    });
+                }
+            }
         });
     }
     e.preventDefault();
